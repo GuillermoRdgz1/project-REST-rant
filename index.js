d0098f236247eb01924260fc4c3cb8ebf52b1233
guillermo rodriguez <memio96@hotmail.com>
2023-01-21 19:00:04 -0800

    places controller

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 
 
+app.use('/places',require('./controllers/places'))
+
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
@@ -11,5 +14,7 @@ app.get('/', (req, res) => {
 app.get('*', (req,res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
+
+
 app.listen(process.env.PORT)
 
