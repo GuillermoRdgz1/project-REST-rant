9d2ddc9457acf186081499cfceef1cefff8d7e5d
guillermo rodriguez <guillermordgz.96@gmail.com>
2023-01-23 21:07:49 -0800

    adding images

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,9 +8,14 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <img id="error" src="/images/404.jpg" alt="404 Error" />
+                <div>
+                Photo by <a href="https://unsplash.com/@villxsmil?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Luis Villasmil</a> on <a href="https://unsplash.com/images/feelings/funny?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
             </main>
         </Def>
     )
 }
 
 module.exports = error404
+
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img id="muffin" src="/images/choco-muffin.jpg" alt="chocolate chip muffin" />
+                    <div>
+                    Photo by <a href="https://unsplash.com/fr/@foodfaithfit?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Taylor Kiser</a> on <a href="https://unsplash.com/photos/zJR-P9dIGu8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
