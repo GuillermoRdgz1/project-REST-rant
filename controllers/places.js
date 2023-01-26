db36a32712ae05b7a08f928c314071c6f4e87a4f
guillermo rodriguez <guillermordgz.96@gmail.com>
2023-01-25 19:52:06 -0800

    pt 4

--- controllers/places.js
+++ controllers/places.js
@@ -7,13 +7,13 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/thai.jpg'
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/bake.jpg'
       }]
       
     res.render('places/index', { places })
@@ -21,3 +21,8 @@ router.get('/', (req, res) => {
 
 
 module.exports = router
+
+//thai Photo by <a href="https://unsplash.com/@shawnanggg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">shawnanggg</a> on <a href="https://unsplash.com/photos/H82HDdkcd4c?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    
+//bakery good Photo by <a href="https://unsplash.com/@pinarimsi?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Pinar Kucuk</a> on <a href="https://unsplash.com/photos/3-PfUgbyWEs?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,17 +4,27 @@ const Def = require('../default')
 function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
-        <div>
+        <div className='col-sm-4'>
           <h2>{place.name}</h2>
-          <img src={place.pic} alt={place.name}/>
+          <p className="text-center">
+            {place.cuisines}
+          </p>
+          <img className='w-100' src={place.pic} alt={place.name}/>
+          <p className="text-center">
+            Located in {place.city}, {place.state}
+          </p>
+          <p id="thai-cred">Photo by <a href="https://unsplash.com/@shawnanggg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">shawnanggg</a> on <a href="https://unsplash.com/photos/H82HDdkcd4c?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+          <p id="bake-cred">Photo by <a href="https://unsplash.com/@pinarimsi?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Pinar Kucuk</a> on <a href="https://unsplash.com/photos/3-PfUgbyWEs?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
         </div>
       )
     })
     return (
       <Def>
           <main>
-              <h1>PLACES INDEX PAGE</h1>
+              <h1>Cravings? CHECK THESE OUT!</h1>
+              <div className='row'>
               {placesFormatted}
+              </div>
           </main>
       </Def>
   )
