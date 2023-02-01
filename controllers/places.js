c85eb9086ac5c0853dc78fb6c938030d313d58c9
guillermo rodriguez <guillermordgz.96@gmail.com>
2023-01-31 19:37:57 -0800

    places new fix

--- controllers/places.js
+++ controllers/places.js
@@ -1,15 +1,25 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
-
-
- router.get('/new', (req, res) => {
-   res.render('places/new')
- })
 
 
  router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
 })
 
 
