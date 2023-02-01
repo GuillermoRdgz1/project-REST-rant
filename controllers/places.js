ad4df07f10b8d6921236884d391dcda4266f1293
guillermo rodriguez <guillermordgz.96@gmail.com>
2023-02-01 15:31:53 -0800

    new place fix

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
+router.get('/', (req, res) => {
+  res.render('places/index', { places })
+})
 
-
- router.post('/', (req, res) => {
+router.post('/', (req, res) => {
   console.log(req.body)
   if (!req.body.pic) {
     req.body.pic = 'http://placekitten.com/400/400'
@@ -24,9 +26,10 @@ router.get('/new', (req, res) => {
 })
 
 
+
+
 module.exports = router
 
 //thai Photo by <a href="https://unsplash.com/@shawnanggg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">shawnanggg</a> on <a href="https://unsplash.com/photos/H82HDdkcd4c?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
     
 //bakery good Photo by <a href="https://unsplash.com/@pinarimsi?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Pinar Kucuk</a> on <a href="https://unsplash.com/photos/3-PfUgbyWEs?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-  
