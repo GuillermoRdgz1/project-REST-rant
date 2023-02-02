25904cbf57421e378c633538a3d5ea77716d63a2
guillermo rodriguez <guillermordgz.96@gmail.com>
2023-02-01 18:53:03 -0800

    fill show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,18 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
+            <img id="show" src={data.place.pic} alt={data.place.name}/>
+            <h2>Rating</h2>
+            <p>Not Rated</p>
+            <div id="comments">
+                <h2>Comments</h2>
+                    <p>No comments yet!</p>
+            </div>
           </main>
         </Def>
     )
